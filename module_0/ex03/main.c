3dd731b1583d6fc156f55f9478e3780b023fb51e
Nelson Júnior <1191342@isep.ipp.pt>
2025-09-22 23:00:36 +0100

    module_0

---
+++ module_0/ex03/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "mul.h"
+
+
+int main(){
+	
+	int a = 5;
+	int b = 2;
+	
+	int result = mul(a,b);
+	
+	printf("multiplication:%d\n",result);
+	
+	return 0;
+}
---
+++ module_0/ex03/mul.c
@@ -0,0 +1,4 @@
+int mul(int a, int b){
+	int multiplication = a * b;
+	return multiplication;
+}
